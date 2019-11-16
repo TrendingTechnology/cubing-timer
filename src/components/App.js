686efe0578ea21b0968f8c4083dd9cc46221e67c
Roger Hiu Fung Cheng <rogerhiufungcheng@gmail.com>
2019-11-16 17:03:34 +1100

    Basic Narbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Route,NavLink,HashRouter} from "react-router-dom";
 import './App.css';
+import Narbar from './narbar'
+import TimerPage from '../containers/timerPage'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <HashRouter>
+      <Narbar />
+      <div className="content">
+        <Route exact path="/" component={TimerPage}/>
+      </div>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/components/narbar.js
@@ -0,0 +1,22 @@
+import './narbar.css'
+
+import React from 'react';
+import {NavLink,HashRouter}from 'react-router-dom'
+
+const Narbar = props =>(
+<nav class="site-header sticky-top py-1">
+  <div class="container d-flex flex-column flex-md-row justify-content-between">
+    <HashRouter>
+      <NavLink exact to="/" className="py-2 d-none d-md-inline-block">Timer</NavLink>
+      <span></span>
+      <NavLink exact to="/" className="py-2 d-none d-md-inline-block">Solves</NavLink>
+      <span></span>
+      <NavLink exact to="/" className="py-2 d-none d-md-inline-block">Stats</NavLink>
+      <span></span>
+      <NavLink exact to="/" className="py-2 d-none d-md-inline-block">Options</NavLink>
+      <span></span>
+    </HashRouter>
+  </div>
+</nav>
+);
+export default Narbar
