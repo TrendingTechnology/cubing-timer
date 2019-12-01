2b35f5cb1e5891b614260ed6eddfb7b6050287ee
XPS13Window <rogerhiufungcheng@gmail.com>
2019-12-01 12:02:10 +1100

    Grid Layout Responsive and Store Setting

--- src/containers/appContainer.js
+++ src/containers/appContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // import {Route,HashRouter} from "react-router-dom";
-import GridLayout from 'react-grid-layout';
+import { Responsive, WidthProvider } from 'react-grid-layout';
 
 // import Narbar from '../components/narbar'
 import Footer from '../components/footer'
@@ -9,30 +9,53 @@ import ListTimes from '../components/listTimes'
 import TimerPage from './timerPage'
 import SolvingPage from './solvingPage'
 
-
+const originalLayouts = getFromLS("layouts") || {};
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 class AppContainer extends Component{
+  static defaultProps = {
+    className:"layout",
+    breakpoints:{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}, //Responsive Breakpoints
+    cols:{lg: 12, md: 10, sm: 6, xs: 4, xxs: 2}, //Responsive Cols
+    verticalCompact: false,  //Free Moment
+  };
   constructor(){
     super()
     this.state={
+      layouts: JSON.parse(JSON.stringify(originalLayouts))
     }
   }
+  resetLayout() {
+    this.setState({ layouts: {} });
+  }
+  onLayoutChange(layout, layouts) {
+    saveToLS("layouts", layouts);
+    this.setState({ layouts });
+  }
+
   render(){
     if(!this.props.status.focus){
       return (
         <div>
-          <GridLayout className="content layout" cols={12} width={1200}>
-          <div key="a" data-grid={{x: 0, y: 0, w: 4, h: 4, minW:4,minH:4}}>
-            <TimerPage />
-          </div>
-          <div key="b" data-grid={{x: 0, y: 0, w: 4, h: 2, minW:4,minH:4}}>
-            <ListTimes />
-          </div>
-          <div key="c" data-grid={{x: 5, y: 5, w: 4, h: 2, minW:1,minH:1}}>
-            c
-          </div>
-
-          </GridLayout>
+          <button onClick={() => this.resetLayout()}>Reset Layout</button>
+          <ResponsiveGridLayout
+            className="layout"
+            layouts={this.state.layouts}
+            onLayoutChange={(layout, layouts) =>
+              this.onLayoutChange(layout, layouts)
+            }
+            {...this.props}
+          >
+            <div key="a" data-grid={{x: 0, y: 0, w: 4, h: 2, minW:4,minH:2}}>
+              <TimerPage />
+            </div>
+            <div key="b" data-grid={{x: 0, y: 0, w: 4, h: 2, minW:4,minH:4}}>
+              <ListTimes />
+            </div>
+            <div key="c" data-grid={{x: 5, y: 5, w: 4, h: 2, minW:1,minH:1}}>
+              c
+            </div>
+          </ResponsiveGridLayout>
           <Footer />
         </div>
       );
@@ -43,6 +66,31 @@ class AppContainer extends Component{
     }
   }
 }
+
+
+function getFromLS(key) {
+  let ls = {};
+  if (global.localStorage) {
+    try {
+      ls = JSON.parse(global.localStorage.getItem("rgl-8")) || {};
+    } catch (e) {
+      /*Ignore*/
+    }
+  }
+  return ls[key];
+}
+
+function saveToLS(key, value) {
+  if (global.localStorage) {
+    global.localStorage.setItem(
+      "rgl-8",
+      JSON.stringify({
+        [key]: value
+      })
+    );
+  }
+}
+
 function mapStateToProps(state) {
   return {
     // timer: state.timer,
